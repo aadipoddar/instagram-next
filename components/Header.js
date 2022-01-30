261dce89f5d5fcda8cd902eef9f968265c18de01
Aadi Poddar <aadipoddarmail@gmail.com>
2022-01-31 00:57:29 +0530

    Designed Middel Section of Header
    Designed Search Button
    Added Tailwind Forms
    Added Hero Icons

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,20 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from 'next/image'
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon,
+} from '@heroicons/react/outline'
 
 function Header() {
     return (
         <div>
             <div className="flex justify-between max-w-6xl">
-                <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
+
+                {/* Left */}
+                <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
                     <Image
                         src="https://links.papareact.com/ocw"
                         layout='fill'
@@ -13,11 +22,26 @@ function Header() {
                     />
                 </div>
 
-                <div className="relative lg:hidden flex-shrink-0 w-10 h-10 cursor-pointer">
+                <div className="relative lg:hidden flex-shrink-0 w-10 cursor-pointer">
                     <Image
                         src="https://links.papareact.com/jjm"
                         layout='fill'
                         objectFit='contain'
+                    />
+                </div>
+
+                {/* Middle - Search Input Field */}
+                <div className="relative mt-1 p-3 rounded-md">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                        <SearchIcon
+                            className="h-5 w-5 text-gray-500"
+                        />
+                    </div>
+
+                    <input
+                        className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+                        type='text'
+                        placeholder="Search"
                     />
                 </div>
             </div>
