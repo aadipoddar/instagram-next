a57ce2a59a86df0dfe3c638df6244b2f84fb0ab9
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-11 21:11:32 +0530

    Implement Right Section of the Header

--- components/Header.js
+++ components/Header.js
@@ -7,11 +7,12 @@ import {
     PaperAirplaneIcon,
     MenuIcon,
 } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 
 function Header() {
     return (
         <div>
-            <div className="flex justify-between max-w-6xl">
+            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
 
                 {/* Left */}
                 <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
@@ -31,19 +32,47 @@ function Header() {
                 </div>
 
                 {/* Middle - Search Input Field */}
-                <div className="relative mt-1 p-3 rounded-md">
-                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                        <SearchIcon
-                            className="h-5 w-5 text-gray-500"
+                <div className='max-w-xs'>
+                    <div className="relative mt-1 p-3 rounded-md">
+                        <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                            <SearchIcon
+                                className="h-5 w-5 text-gray-500"
+                            />
+                        </div>
+
+                        <input
+                            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+                            type='text'
+                            placeholder="Search"
                         />
                     </div>
+                </div>
 
-                    <input
-                        className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
-                        type='text'
-                        placeholder="Search"
+                {/* Right */}
+                <div className='flex items-center justify-end space-x-4'>
+                    <MenuIcon className='h-10 md:hidden cursor-pointer' />
+
+                    <HomeIcon className='navBtn' />
+
+                    <div className='relative navBtn'>
+                        <PaperAirplaneIcon className='navBtn rotate-45' />
+
+                        <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
+                            3
+                        </div>
+                    </div>
+
+                    <PlusCircleIcon className='navBtn' />
+                    <UserGroupIcon className='navBtn' />
+                    <HeartIcon className='navBtn' />
+
+                    <img
+                        src="https://links.papareact.com/3ke"
+                        alt='profile pic'
+                        className='h-10 rounded-full cursor-pointer'
                     />
                 </div>
+
             </div>
         </div>
     )
