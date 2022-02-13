d5874925ad0fd97be1ab0cf009c73a7ef42e141c
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-13 18:57:43 +0530

    SignIn Page Part 1
    Implemented Google SignIn

---
+++ pages/auth/signin.js
@@ -0,0 +1,27 @@
+import { getProviders, signIn as SignIntoProvider } from 'next-auth/react'
+
+function signin({ providers }) {
+    return (
+        <>
+            {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                    <button onClick={() => SignIntoProvider(provider.id)}>
+                        Sign in with {provider.name}
+                    </button>
+                </div>
+            ))}
+        </>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders()
+
+    return {
+        props: {
+            providers
+        }
+    }
+}
+
+export default signin
