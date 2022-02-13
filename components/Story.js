068dd95aa4beca6037bc18717f6e52bf3b76d53f
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-13 16:12:13 +0530

    Stories Section Part 2

--- components/Story.js
+++ components/Story.js
@@ -1,8 +1,12 @@
 function Story({ image, username }) {
     return (
         <div>
-            <img src={image} alt="profile" />
-            <p>{username}</p>
+            <img
+                className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out'
+                src={image}
+                alt="profile"
+            />
+            <p className='text-xs w-14 truncate text-center'>{username}</p>
         </div>
     )
 }
