452f83b5e053d8dc19d8a10ad645f46842978ee5
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-13 19:10:13 +0530

    Next Auth Authentication Part 2

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,11 @@ import {
     MenuIcon,
 } from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Header() {
+    const { data: session } = useSession()
+
     return (
         <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
             <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
@@ -54,23 +57,32 @@ function Header() {
 
                     <HomeIcon className='navBtn' />
 
-                    <div className='relative navBtn'>
-                        <PaperAirplaneIcon className='navBtn rotate-45' />
+                    {session ? (
+                        <>
+                            <div className='relative navBtn'>
+                                <PaperAirplaneIcon className='navBtn rotate-45' />
 
-                        <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
-                            3
-                        </div>
-                    </div>
+                                <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
+                                    3
+                                </div>
+                            </div>
 
-                    <PlusCircleIcon className='navBtn' />
-                    <UserGroupIcon className='navBtn' />
-                    <HeartIcon className='navBtn' />
+                            <PlusCircleIcon className='navBtn' />
+                            <UserGroupIcon className='navBtn' />
+                            <HeartIcon className='navBtn' />
 
-                    <img
-                        src="https://links.papareact.com/3ke"
-                        alt='profile pic'
-                        className='h-10 rounded-full cursor-pointer'
-                    />
+                            <img
+                                onClick={signOut}
+                                src={session.user.image}
+                                alt='profile pic'
+                                className='h-10 w-10 rounded-full cursor-pointer'
+                            />
+                        </>
+                    ) : (
+                        <button onClick={signIn}>Sign In</button>
+                    )}
+
+
                 </div>
 
             </div>
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,8 +1,15 @@
 import { getProviders, signIn as SignIntoProvider } from 'next-auth/react'
+import Head from 'next/head'
 
 function signin({ providers }) {
     return (
         <>
+            <Head>
+                <title>Sign In</title>
+                <meta name="description" content="Instagram Clone in Next JS" />
+                <link rel="icon" href="/InstagramLogo.png" />
+            </Head>
+
             {Object.values(providers).map((provider) => (
                 <div key={provider.name}>
                     <button onClick={() => SignIntoProvider(provider.id)}>
