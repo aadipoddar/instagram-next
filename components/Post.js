c4fd69a47b0b98ecba84d444fe2a45aa616f144e
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-13 17:17:08 +0530

    input Box of Posts

--- components/Post.js
+++ components/Post.js
@@ -53,6 +53,19 @@ function Post({ id, username, userImg, img, caption }) {
                 {caption}
             </p>
 
+            {/* Input Box */}
+            <form className='flex items-center p-4'>
+                <EmojiHappyIcon className="h-7" />
+
+                <input
+                    type='text'
+                    placeholder='Add a comment...'
+                    className='border-none flex-1 focus:ring-0 outline-none'
+                />
+
+                <button className='font-semibold text-blue-400'>Post</button>
+            </form>
+
         </div>
     )
 }
